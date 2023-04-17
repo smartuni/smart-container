b7f8b8cb65a5132598b79468f483d3d74918f214
Maik Lorenz <lorenz.maik@web.de>
2023-04-17 18:06:44 +0200

    Add first example code to setup UART

--- nodes/gps/main.c
+++ nodes/gps/main.c
@@ -19,9 +19,21 @@
  */
 
 #include <stdio.h>
+#include "periph/uart.h"
+
+void rx_cb(void *arg, uint8_t data) {
+    (void) arg; /* argument not used */
+    printf("uart received -> %d\n", data);
+}
 
 int main(void)
 {
     puts("Hello, RIOT GPS!");
+    int ret = 0;
+    
+    uart_t uart2 = 1;
+    ret = uart_init(uart2, 9600, (uart_rx_cb_t) rx_cb, NULL);
+    printf("uart_init -> %d\n", ret);
+
     return 0;
 }
