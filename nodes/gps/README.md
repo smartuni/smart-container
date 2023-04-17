b5c3547667b1007d0f711055056641dc7e120409
Maik Lorenz <lorenz.maik@web.de>
2023-04-17 13:56:36 +0200

    Added RIOT repo for nodes team

---
+++ nodes/gps/README.md
@@ -0,0 +1,9 @@
+GPS Sensor
+===
+
+Describe here what the application is about.
+
+Usage
+=====
+
+Describe here how to use this application
---
+++ nodes/gps/main.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2023 Maik Lorenz
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       GPS sensor app
+ *
+ * @author      Maik Lorenz <lorenz.maik@web.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    puts("Hello, RIOT GPS!");
+    return 0;
+}
