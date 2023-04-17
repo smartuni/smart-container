cd5f7d5e75dac41ab92a13b055c6365fca94eff4
Cedric Michaelis <cedric.michaelis@haw-hamburg.de>
2023-04-17 16:11:15 +0200

    waterleak sensor

---
+++ nodes/sensor/waterleak/bin/pba-d-01-kw2x/saul_init_devs/init.d
@@ -0,0 +1,14 @@
+/home/guest/Documents/smart-container/nodes/sensor/waterleak/bin/pba-d-01-kw2x/saul_init_devs/init.o: \
+ /home/guest/Documents/smart-container/nodes/RIOT/drivers/saul/init_devs/init.c \
+ /home/guest/Documents/smart-container/nodes/sensor/waterleak/bin/pba-d-01-kw2x/riotbuild/riotbuild.h \
+ /home/guest/Documents/smart-container/nodes/RIOT/core/lib/include/kernel_defines.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h
+
+/home/guest/Documents/smart-container/nodes/sensor/waterleak/bin/pba-d-01-kw2x/riotbuild/riotbuild.h:
+
+/home/guest/Documents/smart-container/nodes/RIOT/core/lib/include/kernel_defines.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h:
