4e5f447993c432896bc80fcee281304a25bac413
Mohamed ElGhamrawy <mhelghamrawy@gmail.com>
2023-05-08 17:32:04 +0200

    Updates to sec_save.

--- security/sec_save/sec_save.c
+++ security/sec_save/sec_save.c
@@ -1,5 +1,6 @@
 #include "stdint.h"
 
+#include "eeprom.h"
 #include "crypto/ciphers.h"
 #include "phydat.h"
 
@@ -7,10 +8,20 @@
 #include "sec_save_sdcard.h"
 #include "sec_save.h"
 
+#define AES_KEY_EEPROM_ADDRESS 0x0f
+#define AES_KEY_SIZE 16
+#define AES_BLOCK_SIZE 16
 #define SERIALIZED_PHYDAT_SIZE 512
 #define SERIALIZED_PHYDAT_DIM 1
-#define AES_KEY_SIZE 128
-#define AES_BLOCK_SIZE 128
+
+int read_aes_key(uint8_t *key)
+{
+    if (eeprom_read(AES_KEY_EEPROM_ADDRESS, key, AES_KEY_SIZE) != AES_KEY_SIZE)
+    {
+        return -1;
+    }
+    return 1;
+}
 
 size_t serialize_phydat(uint8_t *serialized_phydat, const phydat_t *phydat)
 {
@@ -33,9 +44,12 @@ int encrypt_buf(uint8_t *key, uint8_t *plain_text, uint8_t *cipher_text)
         return -1;
     }
 
-    if (cipher_encrypt(&cipher, plain_text, cipher_text) < 0)
+    for (i = 0; i < sizeof(plain_text) / AES_BLOCK_SIZE; +ii)
     {
-        return -1;
+        if (cipher_encrypt(&cipher, plain_text + i * AES_BLOCK_SIZE, cipher_text + i * AES_BLOCK_SIZE) < 0)
+        {
+            return -1;
+        }
     }
 
     return 1;
@@ -55,8 +69,14 @@ int sec_save(phydat_t *phydat)
 
     // Encrypt serialized data buffer
     uint8_t key[AES_KEY_SIZE] = {0};
-    uint8_t plain_text[AES_BLOCK_SIZE] = {0};
-    uint8_t cipher_text[AES_BLOCK_SIZE] = {0};
+    uint8_t plain_text[SERIALIZED_PHYDAT_SIZE] = {0};
+    uint8_t cipher_text[SERIALIZED_PHYDAT_SIZE] = {0};
+
+    if (read_aes_key(&key) < 0)
+    {
+        return -1;
+    }
+
     if (encrypt_buf(&key, &plain_text, &cipher_text) < 0)
     {
         return -1;
