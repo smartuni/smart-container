6b4f88fcda4329cec10738eeaddfecfff055cd7a
SaBhr <sahar.behravesh@gmail.com>
2023-05-14 17:35:27 +0200

    About page

--- webApp/Frontend/riot_frontend/pages/about.js
+++ webApp/Frontend/riot_frontend/pages/about.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import Link from 'next/link'
+import styles from '../styles/about.module.css'
 
 export default function About() {
   return (
     <>
-        <h1>About page</h1>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/contact">Contact</a></li>
+          <li><a href="/map">To Map Page</a></li>
+          <li><a href="#">Log out</a></li>
+        </ul>
+      </nav>
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <h2>Secure Smart Supply Chain</h2>
+          <h3>We build <u>smart containers</u> that:</h3>
+          <ul>
+            <li>Increase transparency and security.</li>
+            <li>Provide real-time information about the cargo.</li>
+            <li>Use standardized IoT technologies to ensure reliability and interoperability among containers.</li>
+          </ul>
+        </div>
+        <div className={styles.right}>
+          <img src="/supply_chain.png" alt="Image" />
+        </div>
+    </div>
     </>
   )
 }
