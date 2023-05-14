9373b539755b6d1b9a5d3d372871db62d578f6ce
SaBhr <sahar.behravesh@gmail.com>
2023-05-14 14:26:23 +0200

    Contact page style

--- webApp/Frontend/riot_frontend/pages/contact.js
+++ webApp/Frontend/riot_frontend/pages/contact.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Link from "next/link";
 import styles from '../styles/contact.module.css'
+import Image from "next/image";
+import img1 from '../public/contact_email_icon.png';
+import img2 from '../public/contact_phone_call_icon.png';
+import img3 from '../public/contact_address_icon.png';
+
 
 export default function Contact() {
   return (
@@ -14,27 +19,21 @@ export default function Contact() {
           </ul>
         </nav>
         <div className={styles.contact}>
-        <div className={styles.section}>
-            <svg viewBox="0 0 20 20" className={styles.icon}>
-            <title>Email</title>
-            </svg>
+          <div className={styles.section}>
+            <Image src={img1} alt="Email Address Icon" className={styles.icon}/>
             <h3>Email</h3>
             <p>contact@example.com</p>
-        </div>
-        <div className={styles.section}>
-            <svg viewBox="0 0 20 20" className={styles.icon}>
-            <title>Phone</title>
-            </svg>
+          </div>
+          <div className={styles.section}>
+            <Image src={img2} alt="Phone Call Icon" className={styles.icon}/>
             <h3>Phone</h3>
-            <p>+1 123-456-7890</p>
-        </div>
-        <div className={styles.section}>
-            <svg viewBox="0 0 20 20" className={styles.icon}>
-            <title>Address</title>
-            </svg>
+            <p>+49 40 42875-8452</p>
+          </div>
+          <div className={styles.section}>
+            <Image src={img3} alt="Address Icon" className={styles.icon}/>
             <h3>Address</h3>
-            <p>123 Main St<br />Anytown, USA 12345</p>
-        </div>
+            <p>HAW Hamburg<br />Berliner Tor 7<br />D-20099 Hamburg, Germany</p>
+          </div>
         </div>
     </div>
   );
