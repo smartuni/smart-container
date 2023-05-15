f306874ab9a1a9b88f362cfeaa875f475e853c6e
Cedric Michaelis <cedric.michaelis@haw-hamburg.de>
2023-05-15 12:54:15 +0200

    removed bin and added ignore

--- nodes/concentrator/bin/feather-nrf52840/application_coap-example/server.d
+++
@@ -1,5 +0,0 @@
-/home/guest/Documents/smartcontainer/smart-container/nodes/concentrator/bin/feather-nrf52840/application_coap-example/server.o: \
- /home/guest/Documents/smartcontainer/smart-container/nodes/concentrator/server.c \
- /home/guest/Documents/smartcontainer/smart-container/nodes/concentrator/bin/feather-nrf52840/riotbuild/riotbuild.h
-
-/home/guest/Documents/smartcontainer/smart-container/nodes/concentrator/bin/feather-nrf52840/riotbuild/riotbuild.h:
