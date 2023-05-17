c3f0a76b0471ace2837d5d7e724cfcd97e317238
Emily Antosch <emilylucia.antosch@haw-hamburg.de>
2023-05-17 14:09:00 +0200

    Starting merge on florian branch

--- webApp/Backend/sensor/views.py
+++ webApp/Backend/sensor/views.py
@@ -80,3 +80,86 @@ class ContainerLocation(APIView):
         ).latest("sensor_time")
         serializer = SensorSerializer(gpsLocation)
         return JsonResponse(serializer.data, safe=False)
+
+
+@csrf_exempt
+def signUp(request):
+    userSignUpObj = json.loads(request.body or "{}")
+    responseObj = ResponseModel()
+
+    username = userSignUpObj["username"] or None
+    email = userSignUpObj["email"] or None
+    password = userSignUpObj["password"] or None
+
+    if username == None or not (1 <= len(username) <= 128):
+        responseObj.errorMsg = (
+            "Username has to be a String with length of 1 - 128 characters."
+        )
+        return HttpResponse(json.dumps(responseObj.__dict__))
+    if email == None or not (5 <= len(email) <= 128) or not "@" in email:
+        responseObj.errorMsg = "Email has to be a String with length of 5 - 128 characters and containing '@'."
+        return HttpResponse(json.dumps(responseObj.__dict__))
+    if password == None or not (8 <= len(password) <= 128):
+        responseObj.errorMsg = (
+            "Password has to be a String with length of 8 - 128 characters."
+        )
+        return HttpResponse(json.dumps(responseObj.__dict__))
+
+    try:
+        user = User.objects.create_user(username, email, password)
+    except:
+        responseObj.errorMsg = "The user seems to be already exists."
+        return HttpResponse(json.dumps(responseObj.__dict__))
+
+    responseObj.data = f"You are singed up as: {user}"
+    return HttpResponse(json.dumps(responseObj.__dict__))
+
+
+@csrf_exempt
+def signIn(request):
+    userSignInObj = json.loads(request.body or "{}")
+    responseObj = ResponseModel()
+
+    username = userSignInObj["username"] or None
+    password = userSignInObj["password"] or None
+
+    if username == None or not (1 <= len(username) <= 128):
+        responseObj.errorMsg = (
+            "Username has to be a String with length of 1 - 128 characters."
+        )
+        return HttpResponse(json.dumps(responseObj.__dict__))
+    if password == None or not (8 <= len(password) <= 128):
+        responseObj.errorMsg = (
+            "Password has to be a String with length of 8 - 128 characters."
+        )
+        return HttpResponse(json.dumps(responseObj.__dict__))
+
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        responseObj.data = "You are logged in"
+        return HttpResponse(json.dumps(responseObj.__dict__))
+    else:
+        responseObj.errorMsg = "Sorry but there is a mismatch with your credentials."
+        return HttpResponse(json.dumps(responseObj.__dict__))
+
+
+@csrf_exempt
+@login_required
+def signOut(request):
+    responseObj = ResponseModel()
+
+    logout(request)
+
+    responseObj.data = "Successfully signed out."
+    return HttpResponse(json.dumps(responseObj.__dict__))
+
+
+# TODO: Change the Response type to JSON
+@csrf_exempt
+def checkSignedIn(request):
+    if request.user.is_authenticated:
+        return HttpResponse("You are currently signed in.")
+    else:
+        return HttpResponse("You are currently NOT signed in.")
