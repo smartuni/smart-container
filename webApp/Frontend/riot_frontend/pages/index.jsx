e62d51a8712a0ff01b1471a73672a84149916d71
Landon Schultze <Landon.Schultze@student.csulb.edu>
2023-05-21 23:33:44 +0200

    Scrolling gif background and centered signins

--- webApp/Frontend/riot_frontend/pages/index.jsx
+++ webApp/Frontend/riot_frontend/pages/index.jsx
@@ -16,45 +16,44 @@ export default function Home() {
 
 
   return (
-    <div className='bg-gray-100 min-h-screen'>
-        <div className='p-4'>
-            <div className='w-full m-auto p-4 border rounded-lg bg-white overflow-y-auto'>
+    <div class="bg-scroll bg-sky-gif h-auto">
+        <div className=' p-40'>
+            <div className='w-full m-auto p-4 border rounded-lg bg-white overflow-y-auto h-screen items-center justify-center'>
 
-                
+                {/*Welcome and RIOT -------------------------------------------- */}
                 <div className='px-4 pt-4'>
                     <h2 className="font-bold text-center">WELCOME</h2>
                     <h2 className="font-bold text-center">TO</h2>
                     
-                        <div className="position-relative text-center">
-                            <span className='font-bold text-blue-700 hover:text-blue-800 pr-10  sm:text-4xl md:text-6xl lg:text-9xl text-6xl'>R</span>
-                            <span className='font-bold text-blue-600  hover:text-blue-800 pr-10  sm:text-4xl md:text-6xl lg:text-9xl text-6xl'>I</span>
-                            <span className='font-bold text-blue-500  hover:text-blue-800 pr-10  sm:text-4xl md:text-6xl lg:text-9xl text-6xl'>O</span>
-                            <span className='font-bold text-blue-400  hover:text-blue-800 sm:text-4xl md:text-6xl lg:text-9xl text-6xl' >T</span>
-                        </div>  
+                    <div className="position-relative text-center">
+                        <span className='font-bold text-blue-700 hover:text-blue-800 pr-10  sm:text-4xl md:text-6xl lg:text-9xl text-6xl'>R</span>
+                        <span className='font-bold text-blue-600  hover:text-blue-800 pr-10  sm:text-4xl md:text-6xl lg:text-9xl text-6xl'>I</span>
+                        <span className='font-bold text-blue-500  hover:text-blue-800 pr-10  sm:text-4xl md:text-6xl lg:text-9xl text-6xl'>O</span>
+                        <span className='font-bold text-blue-400  hover:text-blue-800 sm:text-4xl md:text-6xl lg:text-9xl text-6xl' >T</span>
+                    </div>  
                 </div>
+                {/*-------------------------------------------- */}
+                {/*Boat Gif-------------------------------------------- */}
                 <div>
                     <Image src={boatGif} class="mx-auto"/>
                 </div>
-
                 {/*-------------------------------------------- */}
-                <div className='h-screen items-center justify-center'>
-                    <div className='bg-white  justify-between w-full border p-4 rounded-lg'>
+                {/*Sign in and Sign up Buttons-------------------------------------------- */}
+                <div className='bg-white w-96 border p-4 rounded-lg mx-auto'>
+        
+                    <Link href = '/signin' className="bg-blue-200 hover:bg-blue-300 flex justify-center items-center p-2 rounded-lg cursor-pointer">
+                        <p className="font-bold">Sign In</p>
+                    </Link>
+
+                </div>
+
             
-                        <Link href = '/signin' className="bg-blue-200 hover:bg-blue-300 flex justify-center items-center p-2 rounded-lg cursor-pointer">
-                            <p className="font-bold">Sign In</p>
-                        </Link>
 
-                    </div>
-
+                <div className='bg-white w-96 border p-4 rounded-lg mx-auto'>
                 
-
-                    <div className='bg-white  justify-between w-full border p-4 rounded-lg'>
-                    
-                        <Link href = '/contact' className="bg-blue-200 hover:bg-blue-300 flex justify-center items-center p-2 rounded-lg cursor-pointer">
-                            <p className="font-bold">Sign Up</p>
-                        </Link>
-                    </div>
-
+                    <Link href = '/contact' className="bg-blue-200 hover:bg-blue-300 flex justify-center items-center p-2 rounded-lg cursor-pointer">
+                        <p className="font-bold">Sign Up</p>
+                    </Link>
                 </div>
                 {/*-------------------------------------- */}
 
--- webApp/Frontend/riot_frontend/tailwind.config.js
+++ webApp/Frontend/riot_frontend/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   "./pages/**/*.{js,ts,jsx,tsx,mdx}",
   "./components/**/*.{js,ts,jsx,tsx,mdx}",],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'sky-gif' : "url('../assets/sky-clouds.gif')",
+      }
+    },
   },
   plugins: [],
 }
