730cd7807d30dd6488631f8c0206b78ba5d15852
SaBhr <sahar.behravesh@gmail.com>
2023-05-26 21:13:36 +0200

    Temp chart added.

--- webApp/Frontend/riot_frontend/components/TopCards.jsx
+++ webApp/Frontend/riot_frontend/components/TopCards.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
+import BarChart from "./barchart";
+import TempChart from "../pages/temperature"; 
+
+const tempData = [
+    { time: '12:00', temperature: 23 },
+    { time: '12:30', temperature: 24 },
+    { time: '13:00', temperature: 26 },
+    { time: '13:30', temperature: 25 },
+    { time: '14:00', temperature: 23 },
+  ];
+
 
 const TopCards = () => {
     return (
-        <div className='grid lg:grid-cols-5 gap-4 p-4'>
-            <div className='lg:col-span-1 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg'>
-                <div className='flex flex-col w-full pb-4'>
-                    <p className='text-2xl font-bold'>Current Temp:</p>
-                    
+        <div className='grid lg:grid-cols-5 gap-4 p-4'>       
+            <div className='bg-white flex justify-between w-full border p-4 rounded-lg'>
+                <div className='flex flex-col w-full'>
+                    <div className='flex flex-col w-full'>
+                        <p className='text-2xl font-bold'>Current Temp:</p>                  
+                    </div>
+                    <div className='flex justify-center items-center' style={{ width: '80%' }}>
+                        <TempChart data={tempData} />
+                    </div>
                 </div>
-                <p className='bg-green-200 flex justify-center items-center p-2 rounded-lg'>
+                <p className='bg-green-200 flex justify-center items-center p-2 rounded-lg' style={{ position: "relative", left:"-60px" }}>
                     <span className='text-green-700 text-lg'>20,1C</span>
                 </p>
             </div>
