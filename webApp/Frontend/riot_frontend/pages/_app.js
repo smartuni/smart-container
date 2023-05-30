867b0c3ea8cacdd3070af2f8dcc9b41daf76674f
SaBhr <sahar.behravesh@gmail.com>
2023-05-30 21:19:10 +0200

    removed sidebar from signin and signup pages.

--- webApp/Frontend/riot_frontend/pages/_app.js
+++ webApp/Frontend/riot_frontend/pages/_app.js
@@ -1,11 +1,24 @@
 import '/styles/globals.css'
 import Sidebar from '../components/Sidebar'
+import { useRouter } from 'next/router';
 
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const excludedPages = ['/signin', '/signup'];
+  const isExcludedPage = excludedPages.includes(router.asPath);
+
   return (
-    <Sidebar>
-    <Component {...pageProps} />
-    </Sidebar>
+    <>
+      {isExcludedPage ? (
+        <Component {...pageProps} />
+      ) : (
+        <>
+          <Sidebar >
+          <Component {...pageProps} />
+          </Sidebar>
+        </>
+      )}
+    </>
   )
 }
