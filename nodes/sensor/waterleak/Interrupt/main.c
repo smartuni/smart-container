0b6d96493773e831c2cb268f15f89bd55d332f01
Fabian Schmalenbach <fabian.schmalenbach@gmail.com>
2023-06-12 14:57:25 +0200

    Waterleak detection using GPIO interrupt

---
+++ nodes/sensor/waterleak/Interrupt/main.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2022 HAW Hamburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#include <stdio.h>
+#include "periph/gpio.h"
+#include "board.h"
+#include "ztimer.h"
+
+gpio_t waterleak = GPIO_PIN(1, 8);    // Feather Pin D5
+
+void waterleak_callback (void *arg){
+    (void) arg; /* the argument is not used*/
+
+}
+
+
+int main(void)
+{
+    gpio_init_int(waterleak, GPIO_IN, GPIO_FALLING, waterleak_callback, NULL);
+    puts("GPIOs example.");
+
+    return 0;
+}
