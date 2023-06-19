205b253689651e58c74b4affb3eeddb60804d745
d-parkin <parkindap@gmail.com>
2023-06-19 18:53:51 +0200

    accepts new payload structure

--- webApp/Backend/sensor/mqtt.py
+++ webApp/Backend/sensor/mqtt.py
@@ -3,6 +3,9 @@ import datetime
 import paho.mqtt.client as mqtt
 import json
 import base64
+from .models import Container, owner
+from django.db import IntegrityError, ValidationError
+
 
 # Confiugure as needed
 USERNAME="mqtest2@thingsnet"
@@ -17,33 +20,33 @@ TOPIC = f"v3/{USERNAME}/devices/+/up"
 TTN_MQTT_SERVER = "mobi35.inet.haw-hamburg.de"
 
 
-# def postToDatabase(time, data, dataType, owner):
-#     """
-#     The function then creates a new SensorData object using the SensorData.objects.create() method.
-#     The SensorData object has five fields: id, sensor_type, sensor_data, sensor_time, and owner.
-#     The id field is a UUID field that serves as the primary key for the table.
-#     The sensor_type field is a character field that stores the type of sensor. The sensor_data field is a character field that stores the data from the sensor.
-#     The sensor_time field is a date-time field that stores the time the data was recorded.
-#     The owner field is a foreign key to the Container model, which represents the container that the sensor is attached to.
-#     """
-#     owner_instance = Container.objects.get(container_id=owner)
-#     if time is None or data is None or dataType is None or owner_instance is None:
-#         raise (ValueError("One or more parameters are None"))
-#     try:
-#         entry = SensorData.objects.create(
-#             id=uuid.uuid4(),
-#             sensor_type=dataType,
-#             sensor_data=data,
-#             sensor_time=time,
-#             owner=owner_instance,
-#         )
-#         entry.save()
-#     except ValueError as e:
-#         print(e)
-#     except IntegrityError as e:
-#         print(e)
-#     except ValidationError as e:
-#         print(e)
+def postToDatabase(sensorType, sensorData, time):
+    """
+    The function then creates a new SensorData object using the SensorData.objects.create() method.
+    The SensorData object has five fields: id, sensor_type, sensor_data, sensor_time, and owner.
+    The id field is a UUID field that serves as the primary key for the table.
+    The sensor_type field is a character field that stores the type of sensor. The sensor_data field is a character field that stores the data from the sensor.
+    The sensor_time field is a date-time field that stores the time the data was recorded.
+    The owner field is a foreign key to the Container model, which represents the container that the sensor is attached to.
+    """
+    owner_instance = Container.objects.get(container_id=owner)
+    if time is None or sensorData is None or sensorType is None or owner_instance is None:
+        raise (ValueError("One or more parameters are None"))
+    try:
+        entry = sensorData.objects.create(
+            id=uuid.uuid4(),
+            sensor_type=sensorType,
+            sensor_data=sensorData,
+            sensor_time=time,
+            owner=owner_instance,
+        )
+        entry.save()
+    except ValueError as e:
+        print(e)
+    except IntegrityError as e:
+        print(e)
+    except ValidationError as e:
+        print(e)
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -64,28 +67,23 @@ def on_message(client, userdata, msg):
     print(process_message(msg))
 
 
+# {
+# 	sensor: string
+# 	value: number
+# 	unit: string
+# 	time: string
+# }
+
 # Parsing the payload message for the data we want
 def process_message(msg):
     payload = json.loads(msg.payload.decode("utf-8"))
     # Extract temperature and humidity data
 
-    time = payload['received_at']
-    
-    message = payload['uplink_message']['frm_payload']
-    # Convert base64 encoded string to bytes
-    bytes_data = base64.b64decode(message)
-    # Convert bytes to string
-    decoded_message = bytes_data.decode('utf-8')
+    sensor = payload['sensor']
+    value = payload['value'] + payload['unit']
+    time = payload['time']
 
-    # temperature = payload['uplink_message']['decoded_payload']['temperature']['value']
-    # temperature_unit = payload['uplink_message']['decoded_payload']['temperature']['unit']
-    # temperature_str = str(temperature) + " " + temperature_unit
-
-    # humidity = payload['uplink_message']['decoded_payload']['humidity']['value']
-    # humidity_unit = payload['uplink_message']['decoded_payload']['humidity']['unit']
-    # humidity_str = str(humidity) + " " + humidity_unit
-
-    return time, decoded_message
+    return postToDatabase(sensor, value, time)
     
 
 client = mqtt.Client()
