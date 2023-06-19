eb8f862bbb80517525e23b68ba6e82fe60245488
Landon Schultze <Landon.Schultze@student.csulb.edu>
2023-06-19 17:31:10 +0200

    Update useContainers.js

--- webApp/Frontend/riot_frontend/hooks/useContainers.js
+++ webApp/Frontend/riot_frontend/hooks/useContainers.js
@@ -1,36 +1,44 @@
 import { useEffect, useState } from "react"
+
+import Router from "next/router"
 import axios from "axios";
 // 178.128.192.215:5342
 export const useContainers = async () => {
   const [containers, setContainers] = useState([])
+  var containers_data = []
+  function sendProps() {
+    Router.push({
+      pathname: "/dashboard",
+      query: {
+        containers_data,
+      }
+    });
+  }
 
 
-  const res = await fetch('http://178.128.192.215:80/api/container/')
-  const data = await res.json();
+  useEffect(() => {
+    (async () => {
+      try {
+        // fetch sensors and containers from backend then combine sensors per containers
 
-  return {
-    props: { containers_data: data },
-  };
+        const sensorsRes = await axiosInstance.get('http://178.128.192.215:80/api/sensor/')
+        const containersRes = await axiosInstance.get('http://178.128.192.215:80/api/container/')
+
+        const sensorsData = sensorsRes.data
+
+        const containersData = containersRes.data.map(container => ({
+          ...container,
+          sensors: sensorsData.filter(sensor => sensor.owner === container.container_id)
+        }))
+
+        setContainers(containersData)
+        containers_data = containers
+      } catch { }
+    })()
+  }, [])
+
+  // return containers
+  sendProps()
 }
-//   useEffect(() => {
-//     (async () => {
-//       try {
-//         // fetch sensors and containers from backend then combine sensors per containers
 
-//         const sensorsRes = await axiosInstance.get('http://178.128.192.215:80/api/sensor_list/')
-//         const containersRes = await axiosInstance.get('http://178.128.192.215:80/api/container_list/')
 
-//         const sensorsData = sensorsRes.data
-
-//         const containersData = containersRes.data.map(container => ({
-//           ...container,
-//           sensors: sensorsData.filter(sensor => sensor.owner === container.container_id)
-//         }))
-
-//         setContainers(containersData)
-//       } catch { }
-//     })()
-//   }, [])
-
-//   return containers
-// }
