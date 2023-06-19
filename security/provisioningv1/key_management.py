4d7fc499eb8308a35de25abc5014e01afdae1634
Mose-Salman <mostafa.elamir98@gmail.com>
2023-06-19 09:52:20 +0200

    All AES key sent to the main concentrator node.

--- security/provisioningv1/key_management.py
+++ security/provisioningv1/key_management.py
@@ -1,6 +1,5 @@
 import sqlite3
 import secrets
-import binascii
 import serial
 import time
 import logging
@@ -10,23 +9,35 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 def provision_device(device_id, ser):
     dtls_psk_aes_128_key = secrets.token_hex(16)
     sec_save_aes_key = secrets.token_hex(16)
-    ieee802154_key = secrets.token_hex(16)  
+    ieee802154_key = secrets.token_hex(16)
     lorawan_dev_eui = secrets.token_hex(16)
     lorawan_app_eui = secrets.token_hex(16)
     lorawan_app_key = secrets.token_hex(32)
-  
-    ack = ''
+    
     try:
         cpuid = ser.readline().decode().strip()
         ipv6_addr_concentrator = ser.readline().decode().strip()
 
+        # For all devices including the main concentrator node
         data = (f'{dtls_psk_aes_128_key}'
                 f'{sec_save_aes_key}'
-                f'{ieee802154_key}'
-                f'{lorawan_dev_eui}'
-                f'{lorawan_app_eui}'
-                f'{lorawan_app_key}'
-                f'{ipv6_addr_concentrator}')
+                f'{ieee802154_key}')
+
+        db.execute('INSERT INTO provision (device_id, dtls_psk_aes_128_key, sec_save_aes_key, ieee802154_key) VALUES (?, ?, ?, ?)', 
+                   (device_id, dtls_psk_aes_128_key, sec_save_aes_key, ieee802154_key))
+
+        if device_id == 10:  # for the main concentrator node
+            data += (f'{lorawan_dev_eui}'
+                     f'{lorawan_app_eui}'
+                     f'{lorawan_app_key}'
+                     f'{ipv6_addr_concentrator}')
+
+            # Retrieve all keys from the database
+            keys = db.execute('SELECT dtls_psk_aes_128_key, sec_save_aes_key FROM provision').fetchall()
+
+            # Concatenate all keys
+            for key in keys:
+                data += "".join(key)
 
         ser.write(data.encode())
         while ser.out_waiting > 0:
@@ -41,12 +52,9 @@ def provision_device(device_id, ser):
         provision_device(device_id, ser)  
         return
 
-    db.execute('INSERT INTO provision (device_id, dtls_psk_aes_128_key, sec_save_aes_key, ieee802154_key, lorawan_dev_eui, lorawan_app_eui, lorawan_app_key, ipv6_addr_concentrator) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-               (device_id, dtls_psk_aes_128_key, sec_save_aes_key, ieee802154_key, lorawan_dev_eui, lorawan_app_eui, lorawan_app_key, ipv6_addr_concentrator))
-
 with sqlite3.connect('provision.db') as conn:
     db = conn.cursor()
-    db.execute('CREATE TABLE IF NOT EXISTS provision (device_id INTEGER PRIMARY KEY, dtls_psk_aes_128_key TEXT, sec_save_aes_key TEXT, ieee802154_key TEXT, lorawan_dev_eui TEXT, lorawan_app_eui TEXT, lorawan_app_key TEXT, ipv6_addr_concentrator TEXT)')
+    db.execute('CREATE TABLE IF NOT EXISTS provision (device_id INTEGER PRIMARY KEY, dtls_psk_aes_128_key TEXT, sec_save_aes_key TEXT, ieee802154_key TEXT)')
 
     try:
         with serial.Serial('COM6', 115200, timeout=1) as ser:
