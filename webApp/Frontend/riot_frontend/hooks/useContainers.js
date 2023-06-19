2193d7b4ff0114ea0d2fdf8a8d3098d34ba2718b
Landon Schultze <Landon.Schultze@student.csulb.edu>
2023-06-19 14:28:15 +0200

    Update useContainers.js

--- webApp/Frontend/riot_frontend/hooks/useContainers.js
+++ webApp/Frontend/riot_frontend/hooks/useContainers.js
@@ -1,27 +1,27 @@
 import { useEffect, useState } from "react"
-
+// 178.128.192.215:5342
 export const useContainers = () => {
-    const [containers, setContainers] = useState([])
+  const [containers, setContainers] = useState([])
 
-    useEffect( () => {
-      (async () => {
-        try {
+  useEffect(() => {
+    (async () => {
+      try {
         // fetch sensors and containers from backend then combine sensors per containers
-        
-          const sensorsRes = await axiosInstance.get('/api/sensor_list/')
-          const containersRes = await axiosInstance.get('/api/container_list')
-  
-          const sensorsData = sensorsRes.data
-  
-          const containersData = containersRes.data.map(container => ({
-            ...container,
-            sensors: sensorsData.filter(sensor => sensor.owner === container.container_id)
-          }))
-  
-          setContainers(containersData)
-        } catch {}
-      })()
-    }, [])
+
+        const sensorsRes = await axiosInstance.get('http://178.128.192.215:80/api/sensor/')
+        const containersRes = await axiosInstance.get('http://178.128.192.215:80/api/container/')
+
+        const sensorsData = sensorsRes.data
+
+        const containersData = containersRes.data.map(container => ({
+          ...container,
+          sensors: sensorsData.filter(sensor => sensor.owner === container.container_id)
+        }))
+
+        setContainers(containersData)
+      } catch { }
+    })()
+  }, [])
 
   return containers
 }
