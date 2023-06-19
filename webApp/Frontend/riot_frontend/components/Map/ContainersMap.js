74133d285e63864add947d3af8395e11ec65d22e
Hameedhdhd <Hameedhdhd@gmail.com>
2023-06-19 18:12:49 +0200

    container list on map

--- webApp/Frontend/riot_frontend/components/Map/ContainersMap.js
+++ webApp/Frontend/riot_frontend/components/Map/ContainersMap.js
@@ -1,41 +1,94 @@
+import { useEffect, useState } from "react";
+import axiosInstance from "axios";
 import Map from "@/components/Map/Map";
-import styles from '../../styles/mapPopup.module.css'
+import styles from "../../styles/mapPopup.module.css";
 
 const DEFAULT_CENTER = [53.5511, 9.9937];
-// const markers = [
-//   { name: 'Container 1', location: [53.55587, 10.02439], temp: 32, humidity: 50, acceleration: 30, doorStatus:'Opened', lastOpened: '23/02/23' },
-//   { name: 'Container 2', location: [53.55513463677272, 9.992930792175889], temp: 34, humidity: 30, acceleration: 30, doorStatus:'Closed', lastOpened: '23/02/23'  },
-//   { name: 'Container 3', location: [53.53952514227034, 10.0049117958243], temp: 55,  humidity: 20, acceleration: 30  , doorStatus:'Closed', lastOpened: '23/02/23' },
-// ];
 
+export const ContainersMap = () => {
+  const [containers, setContainers] = useState([]);
 
-export const ContainersMap = ({ containers }) => {
-    return (
-        <Map width="100%" height="400" center={DEFAULT_CENTER} zoom={12}>
-            {({ TileLayer, Marker, Popup }) => (
-                <>
-                    <TileLayer
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                    />
-                    {containers.map((container, index) => (
-                        // render containers data as markers
-                        <Marker key={index} position={marker.location} className="Maker">
-                            <Popup className={styles.popup}>
-                                <p> {container.container_id}</p>
+  useEffect(() => {
+    const fetchContainers = async () => {
+      try {
+        const containerResponse = await axiosInstance.get(
+          "http://178.128.192.215/api/container/"
+        );
+        const sensorResponse = await axiosInstance.get(
+          "http://178.128.192.215/api/sensor/"
+        );
 
-                                {container.sensors.map(sensor => (
-                                    <p key={sensor.sensor_type}>{sensor.sensor_type}: {sensor.sensor_data}</p>
-                                ))}
+        const containersWithSensorData = containerResponse.data.map(
+          (container) => {
+            const sensorData = sensorResponse.data.find(
+              (sensor) => sensor.owner === container.container_id
+            );
 
-                                <p>LastOpened: {sensor.sensor_time}</p>
-                            </Popup>
-                        </Marker>
-                    ))}
+            return {
+              ...container,
+              sensor_data: sensorData,
+            };
+          }
+        );
 
-                </>
-            )}
-        </Map>
-    )
-}
+        setContainers(containersWithSensorData);
+      } catch (error) {
+        console.error("Error fetching containers:", error);
+      }
+    };
 
+    fetchContainers();
+  }, []);
+
+  return (
+    <Map width="100%" height={400} center={DEFAULT_CENTER} zoom={12}>
+      {({ TileLayer, Marker, Popup }) => (
+        <>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          {containers.map((container) => {
+            const {
+              container_id,
+              location,
+              container_content,
+              container_door_closed,
+              container_start,
+              container_destination,
+              container_time,
+              sensor_data,
+            } = container;
+            const position =
+              location && location.length === 2 ? location : DEFAULT_CENTER;
+
+            return (
+              <Marker
+                key={container_id}
+                position={position}
+                className="Marker"
+              >
+                <Popup className={styles.popup}>
+                  <p>Container ID: {container_id}</p>
+                  <p>Container location: {location}</p>
+                  <p>Container content: {container_content}</p>
+                  <p>Container door closed: {container_door_closed}</p>
+                  <p>Container start: {container_start}</p>
+                  <p>Container destination: {container_destination}</p>
+                  <p>Last opened: {container_time}</p>
+                  {sensor_data && (
+                    <>
+                      <p>Sensor type: {sensor_data.sensor_type}</p>
+                      <p>Sensor data: {sensor_data.sensor_data}</p>
+                      <p>Sensor time: {sensor_data.sensor_time}</p>
+                    </>
+                  )}
+                </Popup>
+              </Marker>
+            );
+          })}
+        </>
+      )}
+    </Map>
+  );
+};
