721b74fd2a02a0ab70e1dc97a5de75f71df74a56
Mose-Salman <mostafa.elamir98@gmail.com>
2023-06-20 20:09:38 +0200

    Prevent blindly accepting & update PID

--- security/provisioningv1/main.c
+++ security/provisioningv1/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
-#include "net/ieee802154/submac.h"
 #include "periph/uart.h"
 #include "periph/pm.h" 
 #include "net/gnrc.h"
@@ -14,6 +13,7 @@
 #define MTD_NUM_OF_PAGES 1
 #define UART_PID 1 // Added PID
 #define FLASHPAGE_SIZE 4096 
+#define MAX_RETRIES 3
 
 typedef struct {
     uint8_t device_id; // 8-bit device id [0, n]
@@ -28,14 +28,26 @@ typedef struct {
 
 provisioning_data_t provision;
 mtd_dev_t *mtd_dev = NULL;
+
+
 void check_provisioning() {
     // calculate the start address of the page
     uint32_t addr = FLASH_KEY_PAGE * FLASHPAGE_SIZE;
+    
     uint8_t buffer[sizeof(provisioning_data_t)];
     mtd_read(mtd_dev, buffer, addr, sizeof(provisioning_data_t));
 
-    if (memcmp(buffer, &provision, sizeof(provisioning_data_t)) != 0) {
-        printf("Device not provisioned. Please provision the device first!\n");
+    int retries = 0;
+
+    while (memcmp(buffer, &provision, sizeof(provisioning_data_t)) != 0 && retries < MAX_RETRIES) {
+        printf("Device not provisioned. Retrying provisioning...\n");
+        provision_device();
+        mtd_read(mtd_dev, buffer, addr, sizeof(provisioning_data_t));
+        retries++;
+    }
+
+    if (retries >= MAX_RETRIES) {
+        printf("Failed to provision device after %d attempts. Rebooting...\n", MAX_RETRIES);
         pm_reboot();
     }
 }
@@ -53,8 +65,8 @@ void provision_device() {
     printf("ACK\n"); 
 }
 
-void store_ipv6_address(kernel_pid_t pid) {
-    gnrc_netif_t* netif = gnrc_netif_get_by_pid(pid);
+void store_ipv6_address() {
+    gnrc_netif_t* netif = gnrc_netif_iter(NULL);  // get the first network interface
     if (netif == NULL) {
         puts("Error: No network interface found.");
         return;
@@ -69,24 +81,36 @@ void store_ipv6_address(kernel_pid_t pid) {
     // Copy the generated IPv6 address to the provisioning data
    memcpy(provision.ipv6_addr_concentrator, &ipv6_addr, sizeof(ipv6_addr));
 }
+
 static void rx_cb(void *arg, uint8_t data)
 {
     static uint8_t pos = 0;
-    uint8_t *rx_mem = arg;
-    rx_mem[pos] = data;
+    uint8_t *buf = arg;
 
-    if (pos == sizeof(provisioning_data_t) - 1) {
-        memcpy(&provision, rx_mem, sizeof(provisioning_data_t));
-        provision_device();
+    if (pos >= UART_BUFSIZE) {
+        puts("Received data is too large. Ignoring...\n");
         pos = 0;
-    } else {
-        pos++;
+        return;
+    }
+
+    buf[pos++] = data;
+
+    if (data == '\n') {
+        buf[pos] = '\0';  // Null-terminate the string
+        if (strcmp((char*)buf, "S\n") == 0) {
+            store_ipv6_address();
+            provision_device();
+            check_provisioning();
+        }
+        else {
+            check_provisioning();
+        }
+        pos = 0;
     }
 }
 
 int main(void)
 {
-    store_ipv6_address(UART_PID);
     check_provisioning();
 
     uint8_t rx_mem[sizeof(provisioning_data_t)];
