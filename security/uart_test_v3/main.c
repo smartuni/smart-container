8d8c5b75c3973b619467f4acb98f44bc0b7c9a37
Mose-Salman <mostafa.elamir98@gmail.com>
2023-06-21 18:06:37 +0200

    Done

--- security/uart_test_v3/main.c
+++ security/uart_test_v3/main.c
@@ -1,87 +1,54 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdint.h>
+#include <stdlib.h>
 #include "periph/uart.h"
-#include "periph/flashpage.h"
-#include "log.h"
-#include "periph/pm.h"
 
-#define FLASH_KEY_PAGE 240
-#define UART_DEV0 UART_DEV(0)
-#define UART_BUFSIZE  (128U)
+#define UART_DEV          UART_DEV(0)
+#define UART_BAUDRATE     115200
+#define UART_BUFSIZE      128U
 
 typedef struct {
-    uint8_t device_id; 
+    uint8_t device_id;
     uint8_t dtls_psk_aes_128_key[16];
 } provisioning_data_t;
 
 provisioning_data_t provision;
 
-void panic() {
-    while (1) {}
-}
-
-void check_provisioning() {
-    uint8_t* key_ptr = (uint8_t*)flashpage_addr(FLASH_KEY_PAGE);
-    uint8_t* end_ptr = key_ptr + sizeof(provision);
-    uint8_t* provision_ptr = (uint8_t*)&provision;
-
-    for (; key_ptr < end_ptr; key_ptr++, provision_ptr++) {
-        if (*provision_ptr == 0xFF) continue;
-        if (*key_ptr != *provision_ptr) {
-            LOG_ERROR("Device not provisioned. Please provision the device first!\n");
-            pm_reboot();  //panic()
-        }
-    }
-}
-
-void provision_device() {
-    flashpage_erase(FLASH_KEY_PAGE);
-    
-    if(flashpage_write_raw(FLASH_KEY_PAGE, &provision, sizeof(provision)) < 0){
-        LOG_ERROR("Failed to write to flash page!\n");
-        pm_reboot();  
-    }
-
-    provisioning_data_t verify_provision;
-    memcpy(&verify_provision, (uint8_t*)flashpage_addr(FLASH_KEY_PAGE), sizeof(provision));
-    if (memcmp(&verify_provision, &provision, sizeof(provision)) != 0) {
-        LOG_ERROR("Provisioning failed!\n");
-        pm_reboot();  
-    }
-
-    LOG_INFO("Provisioning done successfully!\n");
-    printf("ACK\n"); 
-}
-
 static void rx_cb(void *arg, uint8_t data)
 {
     static uint8_t pos = 0;
-    uint8_t *rx_mem = arg;
-    rx_mem[pos] = data;
+    uint8_t *buf = arg;
 
-    if (pos == sizeof(provisioning_data_t) - 1) {
-        memcpy(&provision, rx_mem, sizeof(provisioning_data_t));
-        provision_device();
+    buf[pos++] = data;
+
+    if (data == '\n') {
+        buf[pos] = '\0';  // Null-terminate the string
+
+        // Parse the device id and AES key
+        char *token = strtok((char*)buf, ",");
+        provision.device_id = atoi(token);
+        token = strtok(NULL, ",");
+        for(int i=0; i<16; i++) {
+            sscanf(&token[i*2], "%02hhx", &provision.dtls_psk_aes_128_key[i]);
+        }
+        printf("Received Device ID: %d, AES key: ", provision.device_id);
+        for (int i = 0; i < 16; i++) {
+            printf("%02x", provision.dtls_psk_aes_128_key[i]);
+        }
+        printf("\n");
+
         pos = 0;
-    } else {
-        pos++;
+        printf("ACK\n");  // Acknowledge the successful provisioning
     }
 }
 
 int main(void)
-{   
-    (void) getchar();
-    check_provisioning();
-
-    uint8_t rx_mem[sizeof(provisioning_data_t)];
+{
+    uint8_t rx_mem[UART_BUFSIZE];
     
-    if (uart_init(UART_DEV0, 115200, rx_cb, rx_mem) < 0) {
-        LOG_ERROR("Cannot initialize UART\n");
+    if (uart_init(UART_DEV, UART_BAUDRATE, rx_cb, rx_mem) < 0) {
+        printf("Cannot initialize UART\n");
         return 1;
     }
     return 0;
 }
-
-
-
