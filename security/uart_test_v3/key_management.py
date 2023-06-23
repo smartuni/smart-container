4c5a9b1d3df9baf5635ec50954905b077c881a98
Mostafa Salman <mostafa.elamir98@gmail.com>
2023-06-23 22:09:02 +0200

    UART Test

--- security/uart_test_v3/key_management.py
+++ security/uart_test_v3/key_management.py
@@ -1,24 +1,19 @@
 import serial
-import secrets
-SERIAL_PORT = '/dev/tty.usbmodem14401'  
-BAUD_RATE = 96000
+import time
 
+SERIAL_PORT = '/dev/tty.usbmodem14201'  
+BAUD_RATE = 96000 
+BAUD_RATE = 115200
 
-def generate_key():
-    return secrets.token_bytes(16)
-
-def send_key(key):
+def send_char(char):
     try:
-        # Open the serial port
         with serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1) as ser:
-            # Send the key
-            ser.write(key)
-
-        print(f"Successfully sent key: {key}")
+            time.sleep(1) # To ensure the connection is established
+            ser.write(char.encode())
+            print(f"Successfully sent: '{char}'")
 
     except serial.SerialException as e:
         print(f"An error occurred: {str(e)}")
 
 if __name__ == "__main__":
-    key = generate_key()
-    send_key(key)
+    send_char('A')
--- security/uart_test_v3/main.c
+++ security/uart_test_v3/main.c
@@ -1,33 +1,23 @@
 #include "periph/uart.h"
-#include <string.h>
 #define UART_DEV0      UART_DEV(0)
-#define UART_BAUD_RATE 96000
-#define RX_BUFFER_SIZE 256
-
-char rx_buffer[RX_BUFFER_SIZE];
-int rx_idx = 0;
+#define UART_BAUD_RATE 115200
 
 void uart_rx_cb(void *ctx, uint8_t data) {
     (void)ctx;
 
-    // Add received data to buffer
-    rx_buffer[rx_idx++] = data;
-
-    // Check for end of transmission or buffer overflow
-    if (data == '\n' || rx_idx == RX_BUFFER_SIZE) {
-        // Null-terminate the string
-        rx_buffer[rx_idx] = '\0';
-
-        // Reset index for next message
-        rx_idx = 0;
-
-        printf("Received key: %s\n", rx_buffer);
+    if (data == 'A') {
+        printf("Received 'A' successfully\n");
+    } else {
+        printf("Received unexpected data: '%c'\n", data);
     }
 }
 
 int main(void) {
-    memset(rx_buffer, 0, sizeof(rx_buffer));
-    uart_init(UART_DEV0, UART_BAUD_RATE, uart_rx_cb, NULL);
+    if(uart_init(UART_DEV0, UART_BAUD_RATE, uart_rx_cb, NULL) < 0) {
+        printf("Cannot initialize UART\n");
+        return 1;
+    }
+
+    printf("Waiting for data...\n");
     return 0;
 }
-
