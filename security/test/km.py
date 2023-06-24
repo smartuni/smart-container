d76ba0e9abc0192459f04520a6ff15d85e7610d2
Mostafa Salman <mostafa.elamir98@gmail.com>
2023-06-25 00:51:44 +0200

    TEST MTD

--- security/test/km.py
+++ security/test/km.py
@@ -5,17 +5,12 @@ import serial
 ser = serial.Serial('/dev/tty.usbmodem14101', 115200, timeout=1)
 time.sleep(2)  # Wait
 
-for i in range(5):  
-    key = secrets.token_hex(16)  # AES-128 key
-    ser.write((key + '\n').encode())  # Send key
-    print(f"Sent key #{i+1}: '{key}'")
-    
-    # Wait for 'ACK' from device
-    ack = ser.readline().decode().strip()
-    if ack == 'ACK':
-        print("ACK received from device\n")
-    else:
-        print("No ACK received. Something went wrong!\n")
+# Generate AES key
+aes_key = secrets.token_hex(16)
+device_id = 1  # device ID (1-10)
 
-    time.sleep(1)  # Wait between sending keys
+# Send the device ID and AES key
+ser.write(f"{device_id}\n".encode())
+time.sleep(1)
+ser.write(f"{aes_key}\n".encode())
 ser.close()
